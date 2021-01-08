a39c1d9617d01578e12890e9c941ca69dcce19e5
harisbr <harisbr@maestralsolutions.com>
2021-01-08 16:53:15 +0100

    final

--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -1,27 +1,37 @@
-import React from "react";
-import Moment from "react-moment";
-import PropTypes from "prop-types";
+import React from 'react';
+import { connect } from 'react-redux';
+import Moment from 'react-moment';
+import PropTypes from 'prop-types';
+import { deleteLog, setCurrent } from '../../actions/logActions';
 
-const LogItem = ({ log }) => {
+import M from 'materialize-css/dist/js/materialize.min.js';
+
+const LogItem = ({ log, deleteLog, setCurrent }) => {
+  const onDelete = () => {
+    deleteLog(log.id);
+
+    M.toast({ html: 'Log deleted' });
+  };
   return (
-    <li className="collection-item">
+    <li className='collection-item'>
       <div>
         <a
-          href="#edit-log-modal"
+          href='#edit-log-modal'
           className={`modal-trigger ${
-            log.attention ? "red-text" : "blue-text"
+            log.attention ? 'red-text' : 'blue-text'
           }`}
+          onClick={() => setCurrent(log)}
         >
           {log.message}
         </a>
         <br />
-        <span className="grey-text">
-          <span className="black-text">ID #{log.id}</span> last updated by{" "}
-          <span className="black-text">{log.tech}</span> on{" "}
-          <Moment format="MMMM Do YYYY, h:mm:ss">{log.date}</Moment>
+        <span className='grey-text'>
+          <span className='black-text'>ID #{log.id}</span> last updated by{' '}
+          <span className='black-text'>{log.tech}</span> on{' '}
+          <Moment format='MMMM Do YYYY, h:mm:ss'>{log.date}</Moment>
         </span>
-        <a className="secondary-content" href="#!">
-          <i className="material-icons grey-text">delete</i>
+        <a className='secondary-content' href='#!' onClick={onDelete}>
+          <i className='material-icons grey-text'>delete</i>
         </a>
       </div>
     </li>
@@ -30,6 +40,7 @@ const LogItem = ({ log }) => {
 
 LogItem.propTypes = {
   log: PropTypes.object.isRequired,
+  deleteLog: PropTypes.func.isRequired,
 };
 
-export default LogItem;
+export default connect(null, { deleteLog, setCurrent })(LogItem);
--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,36 +1,29 @@
-import React, { useState, useEffect } from "react";
-import LogItem from "./LogItem";
-import Preloader from "../layout/Preloader";
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import LogItem from './LogItem';
+import Preloader from '../layout/Preloader';
+import { getLogs } from '../../actions/logActions';
 
-const Logs = () => {
-  const [logs, setLogs] = useState([]);
-  const [loading, setLoading] = useState(false);
-
+// we got the 'log' from the mapStateToProps
+// it equals to content of the initalState from logReducer
+const Logs = ({ log: { logs, loading }, getLogs }) => {
   useEffect(() => {
     getLogs();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const getLogs = async () => {
-    setLoading(true);
-
-    const res = await fetch("/logs");
-    const data = await res.json();
-
-    setLogs(data);
-    setLoading(false);
-  };
-
-  if (loading) {
+  if (loading || logs === null) {
     return <Preloader />;
   }
 
   return (
-    <ul className="collection with-header">
-      <li className="collection-header">
-        <h4 className="center">System Logs</h4>
+    <ul className='collection with-header'>
+      <li className='collection-header'>
+        <h4 className='center'>System Logs</h4>
       </li>
       {!loading && logs.length === 0 ? (
-        <p className="center">No logs to show...</p>
+        <p className='center'>No logs to show...</p>
       ) : (
         logs.map((log) => <LogItem log={log} key={log.id} />)
       )}
@@ -38,4 +31,12 @@ const Logs = () => {
   );
 };
 
-export default Logs;
+Logs.propTypes = {
+  log: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  log: state.log,
+});
+
+export default connect(mapStateToProps, { getLogs })(Logs);
